04b9570c541ecc8fbe398440a42f6ab5ea8e40fc
AlejandroMoreira <Alejandromoreira1996@gmail.com>
2020-09-21 17:06:04 +0200

    Fuentes añadidas

--- lib/views/category_view.dart
+++ lib/views/category_view.dart
@@ -6,16 +6,23 @@ import '../widgets/category_item.dart';
 class CategoryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (e) => CategoryItem(e.title, e.color),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeal"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (e) => CategoryItem(e.title, e.color),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -6,19 +6,27 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(){
+    Navigator.
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(this.title),
-      color: this.color,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.7), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      onTap: selectCategory,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(this.title,style: Theme.of(context).textTheme.headline1),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
